70fe3cc7ff06a814b7c938155ba7b974296e28f5
RISHIKESH KHARADE <rishikeshkharade19@gmail.com>
2024-11-26 22:17:19 +0530

    UC2-Checking Equality of two Lines

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,38 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Line Comparison Computation Program");
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the co-ordinates of x1: ");
+        System.out.println("Enter the co-ordinates of Line1 x1: ");
         double x1 = sc.nextDouble();
-        System.out.println("Enter the co-ordinates of y1: ");
+        System.out.println("Enter the co-ordinates of Line1 y1: ");
         double y1 = sc.nextDouble();
 
-        System.out.println("Enter the co-ordinates of x2: ");
+        System.out.println("Enter the co-ordinates of Line1 x2: ");
         double x2 = sc.nextDouble();
-        System.out.println("Enter the co=ordinates of y2: ");
+        System.out.println("Enter the co=ordinates of Line1 y2: ");
         double y2 = sc.nextDouble();
 
-        double length=Math.sqrt(Math.pow(x2-x1, 2)+Math.pow(y2-y1, 2));
-        System.out.println("The length of the line is: " + length);
+        double length1=Math.sqrt(Math.pow(x2-x1, 2)+Math.pow(y2-y1, 2));
+        System.out.println("The length of the line is: " + length1);
 
 
+        System.out.println("Enter the co-ordinates of Line2 x3: ");
+        double x3=sc.nextDouble();
+        System.out.println("Enter the co-ordinates of Line2 y3: ");
+        double y3=sc.nextDouble();
 
-    }
-}
+        System.out.println("Enter the co-ordinates of Line2 x4: ");
+        double x4=sc.nextDouble();
+        System.out.println("Enter the co-ordinates of Line2 y4: ");
+        double y4= sc.nextDouble();
+
+        double length2=Math.sqrt(Math.pow(x4-x3,2)+Math.pow(y4-y3,2));
+        System.out.println("The length of the line is: "+length2);
+
+        if(Double.valueOf(length1).equals(length2)) {
+            System.out.println("Two lines are equal in length");
+        }else {
+            System.out.println("Two lines are not equal in length");
+        }
+        sc.close();
+        }
+        }
